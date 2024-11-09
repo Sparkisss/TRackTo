4efe6ab91af309904f3cd3769565b1ca4f107e99
Igor Siamykin <sparkisisss@gmail.com>
2024-11-09 15:51:50 +0300

    feat: add authorization on backend

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,8 +1,15 @@
+const tokenService = require('../service/token-service')
 const userService = require('../service/user-service')
+const {validationResult} = require('express-validator')
+const ApiError = require('../exceptions/api-error')
 
 class UserController {
     async registration(req, res, next) {
         try {
+            const errors = validationResult(req)
+            if(!errors.isEmpty()) {
+                return next(ApiError.BadRequest('Ошибка валидации', errors.array()))
+            }
             const {email, password} = req.body
             const userData = await userService.registration(email, password)
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true })
@@ -25,21 +32,36 @@ class UserController {
 
     async logout(req, res, next) {
         try {
-            
+            const { refreshToken } = req.cookies;
+
+            if (!refreshToken) {
+                console.error('Refresh token not provided');
+                return res.status(400).json({ message: 'Refresh token not provided' });
+            }
+
+            const token = await userService.logout(refreshToken);
+            console.log('Token removed:', token);
+
+            res.clearCookie('refreshToken');
+            return res.json({ message: 'Logout successful' });
         } catch (error) {
             next(error)
         }
     }
     async refresh(req, res, next) {
         try {
-            
+            const {refreshToken} = req.cookies
+            const userData = await userService.refresh(refreshToken)
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true })
+            return res.json(userData)
         } catch (error) {
             next(error)
         }
     }
     async getUsers(req, res, next) {
         try {
-            res.json(['1','2','3'])
+            const users = await userService.getAllUsers()
+            return res.json(users)
         } catch (error) {
             next(error)
         }
--- server/router/index.js
+++ server/router/index.js
@@ -1,12 +1,17 @@
 const Router = require('express').Router
 const userController = require('../controllers/user-controller')
-
 const router = new Router()
+const {body} = require('express-validator')
+const authMiddleware = require('../middlewares/auth-middleware')
 
-router.post('/', userController.registration) //registration
+router.post('/', 
+    body('email').isEmail(),
+    body('password').isLength({min: 3, max: 16}),
+    userController.registration
+) 
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 router.get('/refresh', userController.refresh)
-router.get('/users', userController.getUsers)
+router.get('/users', authMiddleware,  userController.getUsers)
 
 module.exports = router
