2bc3e1bd90b5d32591ba9a4d66709c16c121200c
Igor Siamykin <sparkisisss@gmail.com>
2025-01-26 19:52:55 +0300

    fix: hot fix

--- client/src/pages/Dashboard/ui/Dashboard.tsx
+++ client/src/pages/Dashboard/ui/Dashboard.tsx
@@ -2,18 +2,12 @@ import { useEffect, useState } from "react"
 import { SideBar } from "@/widgets/Sidebar/ui/SideBar"
 import { DashboardChart } from "@/features/dashboard/DashboardChart"
 import { Loader } from "@/shared/ui/loader/Loader"
-import { fetchTasks } from "@/entities/tasks/model/slice/taskSlice"
-import { useAppDispatch, useAppSelector } from "@/features/auth"
+import { useAppSelector } from "@/features/auth"
 import { RootState } from "@/app/store/store"
 
 export function Dashboard() {
-  const dispatch = useAppDispatch()
   const { tasks, isLoading, error } = useAppSelector((state: RootState) => state.tasks)
   const [dat, setDat] = useState<{ in: number; title: string }[]>([])
-
-  useEffect(() => {
-    dispatch(fetchTasks("http://localhost:7000/tasks"))
-  }, [dispatch])
 
   useEffect(() => {
     if (tasks.length > 0) {
--- client/src/widgets/Sidebar/ui/SideBar.tsx
+++ client/src/widgets/Sidebar/ui/SideBar.tsx
@@ -8,6 +8,7 @@ import type { MenuInfo } from "rc-menu/lib/interface"
 import { Menu } from "antd"
 import module from "./SideBar.module.scss"
 import { useNavigate, useLocation } from "react-router-dom"
+import { useState, useCallback } from "react"
 
 type MenuItem = Required<MenuProps>["items"][number];
 
@@ -18,19 +19,21 @@ const items: MenuItem[] = [
 ]
 
 export function SideBar() {
+  const [current, setCurrent] = useState("1")
   const navigate = useNavigate()
   const location = useLocation()
 
-  const handleClick = (e: MenuInfo) => {    
+  const handleClick = useCallback((e: MenuInfo) => {
     const basePath = location.pathname.split("/").slice(0, -1).join("/")
     const newPath = `${basePath}/${e.domEvent.currentTarget.innerText.toLocaleLowerCase()}`
     navigate(newPath)
-  }
-  
+    setCurrent(e.key)
+  }, [location.pathname, navigate])
+
   return (
     <div className={module.side}>
       <Menu
-        defaultSelectedKeys={["2"]}
+        selectedKeys={[current]}
         mode="inline"
         items={items}
         onClick={handleClick} 
